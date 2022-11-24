f41d16f10f7d80b209681b5878ff95125c0574e3
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-24 15:53:24 +0600

    react router dom firebase daisyui setup done

---
+++ src/Pages/SharedPages/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            daisyUI
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+
+            <li>
+              <Link>Blog</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
