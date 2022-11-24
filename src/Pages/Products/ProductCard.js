06a89958ebcce61372608354f01822c25ded0c5e
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-25 02:43:01 +0600

    product card added

---
+++ src/Pages/Products/ProductCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const ProductCard = ({ product }) => {
+  const {
+    img,
+    location,
+    name,
+    originalPrice,
+    resalePrice,
+    sellerName,
+    usedYear,
+  } = product;
+  return (
+    <div>
+      <div className="card w-96 p-10 bg-white text-black shadow-xl">
+        <figure>
+          <img style={{ height: "250px" }} src={img} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-blue-800">{name}</h2>
+          <p className="text-black">
+            <span className="font-semibold text-black">Location:</span>{" "}
+            {location}
+          </p>
+          <p>
+            <span className="font-semibold">Resale Price</span> : Tk{" "}
+            {resalePrice}
+          </p>
+          <p>
+            <span className="font-semibold">Original Price</span> : Tk{" "}
+            {originalPrice}
+          </p>
+          <p>
+            <span className="font-semibold">Used</span> : {usedYear}{" "}
+          </p>
+          <p className="text-xl font-semibold">Seller Name : {sellerName}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import ProductCard from "./ProductCard";
 const Products = () => {
   const products = useLoaderData();
   console.log(products);
-  return <div></div>;
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-3 ml-8 mt-10 mb-10 p-10">
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product}></ProductCard>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
