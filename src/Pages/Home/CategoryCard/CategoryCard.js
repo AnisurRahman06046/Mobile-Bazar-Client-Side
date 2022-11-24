25ce9dd09f18da7a3b24a5a4d5d5c33d47943de6
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-25 02:06:29 +0600

    category wise products fetched

--- src/Pages/Home/CategoryCard/CategoryCard.js
+++ src/Pages/Home/CategoryCard/CategoryCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const CategoryCard = ({ category }) => {
   const { _id, name } = category;
   console.log(category);
@@ -8,14 +8,16 @@ const CategoryCard = ({ category }) => {
   };
   return (
     <div>
-      <div onClick={handleCard} className="card w-96  bg-blue-100">
-        <div className="card-body">
-          <div className="card-actions "></div>
-          <h1 className="text-center text-blue-600 text-3xl font-bold">
-            {category.name}
-          </h1>
+      <Link to={`products/${_id}`}>
+        <div onClick={handleCard} className="card w-96  bg-blue-100">
+          <div className="card-body">
+            <div className="card-actions "></div>
+            <h1 className="text-center text-blue-600 text-3xl font-bold">
+              {name}
+            </h1>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import LogIn from "../../Pages/LogIn/LogIn";
+import Products from "../../Pages/Products/Products";
 import SignUp from "../../Pages/SignUp/SignUp";
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,12 @@ export const router = createBrowserRouter([
       { path: "/", element: <Home></Home> },
       { path: "/login", element: <LogIn></LogIn> },
       { path: "/signup", element: <SignUp></SignUp> },
+      {
+        path: "/products/:id",
+        element: <Products></Products>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/products/${params.id}`),
+      },
     ],
   },
 ]);
