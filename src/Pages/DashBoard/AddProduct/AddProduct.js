7f9305c0b6b3b42e471e1f63e075fddec887f2f9
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-27 14:26:25 +0600

    my products is implemented on sellers route

--- src/Pages/DashBoard/AddProduct/AddProduct.js
+++ src/Pages/DashBoard/AddProduct/AddProduct.js
@@ -1,38 +1,70 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const AddProduct = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const navigate = useNavigate();
+  const handleProductData = (data) => {
+    console.log(data);
+    fetch("http://localhost:5000/addedproducts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          toast.success("Successfully added");
+          navigate("/dashboard/myproduct");
+        }
+      });
+  };
   return (
     <div>
       <h1>Add A Product</h1>
-      <form className="px-40 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2 p-3 bg-orange-100">
+      <form
+        onSubmit={handleSubmit(handleProductData)}
+        className="px-40 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2 p-3 bg-orange-100"
+      >
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Name</span>
           </label>
           <input
+            {...register("name")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Price</span>
           </label>
           <input
+            {...register("price")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Condition</span>
           </label>
-          <select className="select select-bordered w-full max-w-xs">
+          <select
+            {...register("condition")}
+            className="select select-bordered w-full max-w-xs"
+          >
             <option disabled>Select Condition</option>
             <option>Excellent</option>
             <option>Fair</option>
@@ -40,55 +72,55 @@ const AddProduct = () => {
           </select>
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Mobile Number</span>
           </label>
           <input
+            {...register("phone")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Location</span>
           </label>
           <input
+            {...register("location")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Year of Purchase</span>
           </label>
           <input
+            {...register("purchaseYear")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Image</span>
           </label>
           <input
+            {...register("img")}
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
         <div className="form-control">
-          {" "}
           <label>
             <span className="label-text text-xl">Description</span>
           </label>
           <textarea
+            {...register("description")}
             className="textarea  md:w-[320px] lg:w-[320px] textarea-bordered"
             placeholder="Bio"
           ></textarea>
--- src/Pages/DashBoard/MyProducts/MyProducts.js
+++ src/Pages/DashBoard/MyProducts/MyProducts.js
@@ -1,9 +1,52 @@
 import React from "react";
-
+// import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 const MyProducts = () => {
+  const { data: myProducts = [] } = useQuery({
+    queryKey: ["addedproducts"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/addedproducts");
+      const data = await res.json();
+      return data;
+    },
+  });
   return (
     <div>
       <h1>My products</h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              {/* <th>Payment Status</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {myProducts.map((myProduct, i) => (
+              <>
+                <tr key={myProduct._id}>
+                  <th>{i + 1}</th>
+                  <td>
+                    <img
+                      style={{ height: "80px", width: "100px" }}
+                      src={myProduct.img}
+                      alt=""
+                    />
+                  </td>
+                  <td>{myProduct.name}</td>
+                  <td>{myProduct.price}</td>
+                  {/* <td>
+                    <Link className="btn btn-primary btn-sm">Pay</Link>
+                  </td> */}
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
